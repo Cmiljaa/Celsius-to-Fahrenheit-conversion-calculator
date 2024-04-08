c54e9913900589c889c769a6aa2c5c672aa2bee0
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-04-08 17:27:37 +0200

    Added result paragraph

--- script.js
+++ script.js
@@ -6,31 +6,31 @@ let calculate = document.querySelector('.container .converter #calculate');
 const reversef = () =>{
     p.innerHTML = 'Fahrenheit to Celsius (ºF to ºC) conversion calculator';
     input.setAttribute('placeholder', '°F');
+    document.querySelector('.result').innerHTML = '1°F = 0.55556°C';
 }
 
 const reversec = () =>{
     p.innerHTML = 'Celsius to Fahrenheit (ºC to ºF) conversion calculator';
     input.setAttribute('placeholder', '°C');
+    document.querySelector('.result').innerHTML = '1°C = 33.800°F';
+}
+
+window.onload = () => {
+    let parent = document.querySelector('.container');
+    let np = document.createElement('p');
+    np.setAttribute('class','result')
+    np.innerHTML = '1°C = 33.800°F';
+    parent.appendChild(np);
 }
 
 swap.addEventListener('click',() => reverse());
 
-calculate.addEventListener('click', () => calculatef());
+calculate.addEventListener('click', () => console.log('q'));
 
 const reverse = () => {
     if(input.getAttribute('placeholder') === '°C')
         reversef();
     else
         reversec();
-    removep();
     input.value = '';
-}
-
-const calculatef = () =>{
-    let parent = document.querySelector('.container');
-    parent.lastChild.remove();
-    //parent.removeChild(np);
-    let np = document.createElement('p');
-    np.innerText = 'Jebem ti mamu';
-    parent.appendChild(np);
 }
