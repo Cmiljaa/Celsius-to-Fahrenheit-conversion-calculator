566b9c688b2091c5500bda39b3b78fb3eaef249b
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-04-11 20:46:28 +0200

    Transformed procedural code to oop

--- script.js
+++ script.js
@@ -1,54 +1,65 @@
-let p = document.querySelector('.container .converter p');
-let input = document.querySelector('.container .converter input');
-let swap = document.querySelector('.container .converter #swap');
-let calculate = document.querySelector('.container .converter #calculate');
+class TemperatureConverter{
+    constructor() {
+        this.p = document.querySelector('.container .converter p');
+        this.input = document.querySelector('.container .converter input');
+        this.swap = document.querySelector('.container .converter #swap');
+        this.calculate = document.querySelector('.container .converter #calculate');
+        this.initialization();
+        this.eventListeners();
+    }
 
-window.onload = () => {
-    let parent = document.querySelector('.container');
-    let np = document.createElement('p');
-    np.setAttribute('class','result')
-    np.innerHTML = '1°C = 33.800°F';
-    parent.appendChild(np);
-}
+    initialization = () =>{
+        let parent = document.querySelector('.container');
+        this.np = document.createElement('p');
+        this.np.setAttribute('class','result');
+        this.np.innerHTML = '1°C = 33.800°F';
+        parent.appendChild(this.np);
+    }
 
-const reversef = () =>{
-    p.innerHTML = 'Fahrenheit to Celsius (ºF to ºC) conversion calculator';
-    input.setAttribute('placeholder', '°F');
-    document.querySelector('.result').innerHTML = '1°F = 0.556°C';
-}
-
-const reversec = () =>{
-    p.innerHTML = 'Celsius to Fahrenheit (ºC to ºF) conversion calculator';
-    input.setAttribute('placeholder', '°C');
-    document.querySelector('.result').innerHTML = '1°C = 33.800°F';
-}
-
-const reverse = () => {
-    if(input.getAttribute('placeholder') === '°C')
-        reversef();
-    else
-        reversec();
-    input.value = '';
-}
-
-const calculatef = () =>{
-    if(input.value === '')
-        alert('Your input is empty!');
-    else{
-        let value = parseFloat(input.value);
+    reversef = () =>{
+        this.p.innerHTML = 'Fahrenheit to Celsius (ºF to ºC) conversion calculator';
+        this.input.setAttribute('placeholder', '°F');
+        document.querySelector('.result').innerHTML = '1°F = 0.556°C';
+    }
+    
+    reversec = () =>{
+        this.p.innerHTML = 'Celsius to Fahrenheit (ºC to ºF) conversion calculator';
+        this.input.setAttribute('placeholder', '°C');
+        document.querySelector('.result').innerHTML = '1°C = 33.800°F';
+    }
+    
+    reverse = () => {
         if(input.getAttribute('placeholder') === '°C')
-        {
-            let res = (value * 1.8) + 32;
-            document.querySelector('.result').innerHTML = `${input.value}°C = ${res.toFixed(3)}°F`
-        }
+            this.reversef();
         else
-        {
-            let res = (value - 32) / 1.8;
-            document.querySelector('.result').innerHTML = `${input.value}°F = ${res.toFixed(3)}°C`
+            this.reversec();
+        this.input.value = '';
+    }
+
+    calculatef = () =>{
+        if(this.input.value === '')
+            alert('Your input is empty!');
+        else{
+            let value = parseFloat(this.input.value);
+            if(input.getAttribute('placeholder') === '°C')
+            {
+                let res = (value * 1.8) + 32;
+                document.querySelector('.result').innerHTML = `${input.value}°C = ${res.toFixed(3)}°F`
+            }
+            else
+            {
+                let res = (value - 32) / 1.8;
+                document.querySelector('.result').innerHTML = `${input.value}°F = ${res.toFixed(3)}°C`
+            }
         }
     }
+
+    eventListeners() {
+        swap.addEventListener('click',() => this.reverse());
+
+        calculate.addEventListener('click', () => this.calculatef());
+    }
+
 }
 
-swap.addEventListener('click',() => reverse());
-
-calculate.addEventListener('click', () => calculatef());
+const obj = new TemperatureConverter();
